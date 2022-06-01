a383db4063ea1e3f057f842e519cf8aa5825c548
Marc0Franc0 <marcofranco340@gmail.com>
2022-05-31 22:35:25 -0300

    Update frontend
    Se modifico el componente utilidades

--- frontend/src/app/components/utilidades/utilidades.component.ts
+++ frontend/src/app/components/utilidades/utilidades.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-utilidades',
+  selector: 'utilidades',
   templateUrl: './utilidades.component.html',
   styleUrls: ['./utilidades.component.scss']
 })
