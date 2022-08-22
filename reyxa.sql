0ccba1d0a89621f6a4b6555e3a7046d72d045a5b
Marc0Franc0 <marcofranco340@gmail.com>
2022-08-22 00:04:05 -0300

    Update reyxa.sql

--- reyxa.sql
+++ reyxa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-08-2022 a las 21:07:54
+-- Tiempo de generación: 22-08-2022 a las 05:02:59
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,98 +24,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comandos_sistema`
+-- Estructura de tabla para la tabla `comandos`
 --
 
-CREATE TABLE `comandos_sistema` (
+CREATE TABLE `comandos` (
   `id_comando` int(11) NOT NULL,
   `nombre` varchar(40) COLLATE utf8_bin DEFAULT NULL,
   `id_descripcion` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `comandos_sistema`
+-- Volcado de datos para la tabla `comandos`
 --
 
-INSERT INTO `comandos_sistema` (`id_comando`, `nombre`, `id_descripcion`) VALUES
+INSERT INTO `comandos` (`id_comando`, `nombre`, `id_descripcion`) VALUES
 (1, 'notepad', 1),
 (2, 'explorer', 2),
 (3, 'paint', 3),
 (4, 'calculadora', 4),
-(5, 'Wconfig', 5);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comandos_utilidad`
---
-
-CREATE TABLE `comandos_utilidad` (
-  `id_comando` int(11) NOT NULL,
-  `nombre` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `id_descripcion` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `comandos_utilidad`
---
-
-INSERT INTO `comandos_utilidad` (`id_comando`, `nombre`, `id_descripcion`) VALUES
-(1, 'Programar apagado de pc', 14),
-(2, 'Cancelar apagado de pc', 15),
-(3, 'Analisis con Windows Defender', 16),
-(4, 'Windows Update', 17),
-(5, 'Borrar papelera ', 18),
-(6, 'Borrar papelera ', 19),
-(7, 'Borrar papelera ', 20),
-(8, 'Borrar papelera ', 21),
-(9, 'Borrar papelera ', 22),
-(10, 'Borrar papelera ', 23),
-(11, 'Borrar papelera ', 24),
-(12, 'Borrar papelera ', 25),
-(13, 'Borrar papelera ', 26),
-(14, 'Borrar papelera ', 27),
-(15, 'Borrar papelera ', 28),
-(16, 'Borrar papelera ', 29),
-(17, 'Borrar papelera ', 30),
-(18, 'Borrar papelera ', 31),
-(19, 'Borrar papelera ', 32),
-(20, 'Borrar papelera ', 33),
-(21, 'Borrar papelera ', 34),
-(22, 'Borrar papelera ', 35),
-(23, 'Borrar papelera ', 36),
-(25, 'Borrar papelera ', 37),
-(26, 'Borrar papelera ', 38),
-(27, 'Borrar papelera ', 39),
-(28, 'Borrar papelera ', 40),
-(29, 'Borrar papelera ', 41),
-(30, 'Borrar papelera ', 42);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comandos_web`
---
-
-CREATE TABLE `comandos_web` (
-  `id_comando` int(11) NOT NULL,
-  `nombre` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `id_descripcion` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `comandos_web`
---
-
-INSERT INTO `comandos_web` (`id_comando`, `nombre`, `id_descripcion`) VALUES
-(1, 'clima', 6),
-(2, 'Noticias', 7),
-(3, 'Instagram', 8),
-(4, 'WhatsApp', 9),
-(5, 'Gmail', 10),
-(6, 'Netflix', 11),
-(7, 'Spotify', 12),
-(8, 'Youtube', 13);
+(5, 'wconfig', 5),
+(6, 'clima', 6),
+(7, 'noticias', 7),
+(8, 'instagram', 8),
+(9, 'whatsapp', 9),
+(10, 'gmail', 10),
+(11, 'netflix', 11),
+(12, 'spotify', 12),
+(13, 'youtube', 13),
+(14, 'progapagado', 14),
+(15, 'cancapagado', 15),
+(16, 'antivirus', 16),
+(17, 'wupdate', 17),
+(18, 'papelera1', 18),
+(19, 'papelera2', 19),
+(20, 'papelera3', 20),
+(21, 'papelera4', 21),
+(22, 'papelera5', 22),
+(23, 'papelera6', 23),
+(24, 'papelera7', 24),
+(25, 'papelera8', 25),
+(26, 'papelera9', 26),
+(27, 'papelera10', 27),
+(28, 'papelera11', 28),
+(29, 'papelera12', 29),
+(30, 'papelera13', 30),
+(31, 'papelera14', 31),
+(32, 'papelera15', 32),
+(33, 'papelera16', 33),
+(34, 'papelera17', 34),
+(35, 'papelera18', 35),
+(36, 'papelera19', 36),
+(37, 'papelera20', 37),
+(38, 'papelera21', 38),
+(39, 'papelera22', 39),
+(40, 'papelera23', 40),
+(41, 'papelera24', 41),
+(42, 'papelera25', 42);
 
 -- --------------------------------------------------------
 
@@ -192,7 +156,9 @@ CREATE TABLE `descripcion_opiniones` (
 --
 
 INSERT INTO `descripcion_opiniones` (`id_descripcion`, `descripcion`) VALUES
-(1, 'Nashe');
+(1, 'Nashe'),
+(2, 'Excelente'),
+(3, 'God');
 
 -- --------------------------------------------------------
 
@@ -228,30 +194,18 @@ CREATE TABLE `opiniones` (
 --
 
 INSERT INTO `opiniones` (`id_opinion`, `nombre`, `id_descripcion`) VALUES
-(1, 'Marco', 1);
+(1, 'Marco', 1),
+(2, 'Franco', 2),
+(13, 'Lisandro', 3);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `comandos_sistema`
+-- Indices de la tabla `comandos`
 --
-ALTER TABLE `comandos_sistema`
-  ADD PRIMARY KEY (`id_comando`),
-  ADD KEY `fkdesc` (`id_descripcion`);
-
---
--- Indices de la tabla `comandos_utilidad`
---
-ALTER TABLE `comandos_utilidad`
-  ADD PRIMARY KEY (`id_comando`),
-  ADD KEY `fkdesc` (`id_descripcion`);
-
---
--- Indices de la tabla `comandos_web`
---
-ALTER TABLE `comandos_web`
+ALTER TABLE `comandos`
   ADD PRIMARY KEY (`id_comando`),
   ADD KEY `fkdesc` (`id_descripcion`);
 
@@ -280,22 +234,10 @@ ALTER TABLE `opiniones`
 --
 
 --
--- AUTO_INCREMENT de la tabla `comandos_sistema`
+-- AUTO_INCREMENT de la tabla `comandos`
 --
-ALTER TABLE `comandos_sistema`
-  MODIFY `id_comando` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `comandos_utilidad`
---
-ALTER TABLE `comandos_utilidad`
-  MODIFY `id_comando` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT de la tabla `comandos_web`
---
-ALTER TABLE `comandos_web`
-  MODIFY `id_comando` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `comandos`
+  MODIFY `id_comando` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `descripcion_comandos`
@@ -307,13 +249,13 @@ ALTER TABLE `descripcion_comandos`
 -- AUTO_INCREMENT de la tabla `descripcion_opiniones`
 --
 ALTER TABLE `descripcion_opiniones`
-  MODIFY `id_descripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_descripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `opiniones`
 --
 ALTER TABLE `opiniones`
-  MODIFY `id_opinion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_opinion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
