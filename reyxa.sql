d89617de75a0a85a5b1eab5c6dd18d12eb17678a
Marc0Franc0 <marcofranco340@gmail.com>
2022-10-04 15:13:17 -0300

    Update reyxa.sql

--- reyxa.sql
+++ reyxa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-08-2022 a las 17:47:20
+-- Tiempo de generación: 04-10-2022 a las 20:11:46
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -32,14 +32,14 @@ USE `reyxa`;
 CREATE TABLE `comandos` (
   `id_comando` int(11) NOT NULL,
   `nombre` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `id_descripcion` int(11) NOT NULL
+  `sintaxis_comando` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `comandos`
 --
 
-INSERT INTO `comandos` (`id_comando`, `nombre`, `id_descripcion`) VALUES
+INSERT INTO `comandos` (`id_comando`, `nombre`, `sintaxis_comando`) VALUES
 (1, 'notepad', 1),
 (2, 'explorer', 2),
 (3, 'paint', 3),
@@ -86,80 +86,20 @@ INSERT INTO `comandos` (`id_comando`, `nombre`, `id_descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `descripcion_comandos`
+-- Estructura de tabla para la tabla `comentarios`
 --
 
-CREATE TABLE `descripcion_comandos` (
-  `id_descripcion` int(11) NOT NULL,
-  `descripcion` varchar(80) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `descripcion_comandos`
---
-
-INSERT INTO `descripcion_comandos` (`id_descripcion`, `descripcion`) VALUES
-(1, 'start notepad.exe'),
-(2, 'start explorer.exe'),
-(3, 'start mspaint.exe'),
-(4, 'start calc.exe'),
-(5, 'start ms-settings:'),
-(6, 'start https://www.smn.gob.ar/'),
-(7, 'start https://news.google.com/topstories?hl=es-419&gl=US&ceid=US:es-419'),
-(8, 'start https://www.instagram.com/?theme=dark'),
-(9, 'start https://web.whatsapp.com/'),
-(10, 'start https://mail.google.com/mail/u/0/#inbox'),
-(11, 'start https://www.netflix.com/ar/Login'),
-(12, 'start https://open.spotify.com/'),
-(13, 'start https://www.youtube.com/'),
-(14, 'shutdown -s -t '),
-(15, 'shutdown /a'),
-(16, 'start-MpScan'),
-(17, 'start ms-settings:windowsupdate'),
-(18, 'rd /s /q a:\\\\$Recycle.bin'),
-(19, 'rd /s /q b:\\\\$Recycle.bin'),
-(20, 'rd /s /q c:\\\\$Recycle.bin'),
-(21, 'rd /s /q d:\\\\$Recycle.bin'),
-(22, 'rd /s /q e:\\\\$Recycle.bin'),
-(23, 'rd /s /q f:\\\\$Recycle.bin'),
-(24, 'rd /s /q g:\\\\$Recycle.bin'),
-(25, 'rd /s /q h:\\\\$Recycle.bin'),
-(26, 'rd /s /q i:\\\\$Recycle.bin'),
-(27, 'rd /s /q j:\\\\$Recycle.bin'),
-(28, 'rd /s /q k:\\\\$Recycle.bin'),
-(29, 'rd /s /q l:\\\\$Recycle.bin'),
-(30, 'rd /s /q m:\\\\$Recycle.bin'),
-(31, 'rd /s /q n:\\\\$Recycle.bin'),
-(32, 'rd /s /q o:\\\\$Recycle.bin'),
-(33, 'rd /s /q p:\\\\$Recycle.bin'),
-(34, 'rd /s /q r:\\\\$Recycle.bin'),
-(35, 'rd /s /q s:\\\\$Recycle.bin'),
-(36, 'rd /s /q t:\\\\$Recycle.bin'),
-(37, 'rd /s /q u:\\\\$Recycle.bin'),
-(38, 'rd /s /q v:\\\\$Recycle.bin'),
-(39, 'rd /s /q w:\\\\$Recycle.bin'),
-(40, 'rd /s /q x:\\\\$Recycle.bin'),
-(41, 'rd /s /q y:\\\\$Recycle.bin'),
-(42, 'rd /s /q z:\\\\$Recycle.bin');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `descripcion_opiniones`
---
-
-CREATE TABLE `descripcion_opiniones` (
-  `id_descripcion` int(11) NOT NULL,
-  `descripcion` varchar(40) DEFAULT NULL
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `comentario` varchar(200) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `descripcion_opiniones`
+-- Volcado de datos para la tabla `comentarios`
 --
 
-INSERT INTO `descripcion_opiniones` (`id_descripcion`, `descripcion`) VALUES
-(1, 'Nashe'),
-(2, 'Excelente');
+INSERT INTO `comentarios` (`id_comentario`, `comentario`) VALUES
+(1, 'Nashe');
 
 -- --------------------------------------------------------
 
@@ -176,27 +116,86 @@ CREATE TABLE `hibernate_sequence` (
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(1);
+(8);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `opiniones`
+-- Estructura de tabla para la tabla `sintaxis_de_comandos`
 --
 
-CREATE TABLE `opiniones` (
-  `id_opinion` int(11) NOT NULL,
-  `nombre` varchar(40) DEFAULT NULL,
-  `id_descripcion` int(11) NOT NULL
+CREATE TABLE `sintaxis_de_comandos` (
+  `id_descripcion` int(11) NOT NULL,
+  `sintaxis` varchar(80) COLLATE utf8_bin DEFAULT NULL,
+  `descripcion` varchar(80) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `sintaxis_de_comandos`
+--
+
+INSERT INTO `sintaxis_de_comandos` (`id_descripcion`, `sintaxis`, `descripcion`) VALUES
+(1, 'start notepad.exe', NULL),
+(2, 'start explorer.exe', NULL),
+(3, 'start mspaint.exe', NULL),
+(4, 'start calc.exe', NULL),
+(5, 'start ms-settings:', NULL),
+(6, 'start https://www.smn.gob.ar/', NULL),
+(7, 'start https://news.google.com/topstories?hl=es-419&gl=US&ceid=US:es-419', NULL),
+(8, 'start https://www.instagram.com/?theme=dark', NULL),
+(9, 'start https://web.whatsapp.com/', NULL),
+(10, 'start https://mail.google.com/mail/u/0/#inbox', NULL),
+(11, 'start https://www.netflix.com/ar/Login', NULL),
+(12, 'start https://open.spotify.com/', NULL),
+(13, 'start https://www.youtube.com/', NULL),
+(14, 'shutdown -s -t ', NULL),
+(15, 'shutdown /a', NULL),
+(16, 'start-MpScan', NULL),
+(17, 'start ms-settings:windowsupdate', NULL),
+(18, 'rd /s /q a:\\\\$Recycle.bin', NULL),
+(19, 'rd /s /q b:\\\\$Recycle.bin', NULL),
+(20, 'rd /s /q c:\\\\$Recycle.bin', NULL),
+(21, 'rd /s /q d:\\\\$Recycle.bin', NULL),
+(22, 'rd /s /q e:\\\\$Recycle.bin', NULL),
+(23, 'rd /s /q f:\\\\$Recycle.bin', NULL),
+(24, 'rd /s /q g:\\\\$Recycle.bin', NULL),
+(25, 'rd /s /q h:\\\\$Recycle.bin', NULL),
+(26, 'rd /s /q i:\\\\$Recycle.bin', NULL),
+(27, 'rd /s /q j:\\\\$Recycle.bin', NULL),
+(28, 'rd /s /q k:\\\\$Recycle.bin', NULL),
+(29, 'rd /s /q l:\\\\$Recycle.bin', NULL),
+(30, 'rd /s /q m:\\\\$Recycle.bin', NULL),
+(31, 'rd /s /q n:\\\\$Recycle.bin', NULL),
+(32, 'rd /s /q o:\\\\$Recycle.bin', NULL),
+(33, 'rd /s /q p:\\\\$Recycle.bin', NULL),
+(34, 'rd /s /q r:\\\\$Recycle.bin', NULL),
+(35, 'rd /s /q s:\\\\$Recycle.bin', NULL),
+(36, 'rd /s /q t:\\\\$Recycle.bin', NULL),
+(37, 'rd /s /q u:\\\\$Recycle.bin', NULL),
+(38, 'rd /s /q v:\\\\$Recycle.bin', NULL),
+(39, 'rd /s /q w:\\\\$Recycle.bin', NULL),
+(40, 'rd /s /q x:\\\\$Recycle.bin', NULL),
+(41, 'rd /s /q y:\\\\$Recycle.bin', NULL),
+(42, 'rd /s /q z:\\\\$Recycle.bin', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_de_usuario` varchar(40) DEFAULT NULL,
+  `comentario_usuario` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `opiniones`
+-- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `opiniones` (`id_opinion`, `nombre`, `id_descripcion`) VALUES
-(1, 'Marco', 1),
-(2, 'Franco', 2);
+INSERT INTO `usuarios` (`id_usuario`, `nombre_de_usuario`, `comentario_usuario`) VALUES
+(1, 'Marco', 1);
 
 --
 -- Índices para tablas volcadas
@@ -207,27 +206,27 @@ INSERT INTO `opiniones` (`id_opinion`, `nombre`, `id_descripcion`) VALUES
 --
 ALTER TABLE `comandos`
   ADD PRIMARY KEY (`id_comando`),
-  ADD KEY `fkdesc` (`id_descripcion`);
+  ADD KEY `fkdesc` (`sintaxis_comando`);
 
 --
--- Indices de la tabla `descripcion_comandos`
+-- Indices de la tabla `comentarios`
 --
-ALTER TABLE `descripcion_comandos`
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`);
+
+--
+-- Indices de la tabla `sintaxis_de_comandos`
+--
+ALTER TABLE `sintaxis_de_comandos`
   ADD PRIMARY KEY (`id_descripcion`);
 
 --
--- Indices de la tabla `descripcion_opiniones`
+-- Indices de la tabla `usuarios`
 --
-ALTER TABLE `descripcion_opiniones`
-  ADD PRIMARY KEY (`id_descripcion`);
-
---
--- Indices de la tabla `opiniones`
---
-ALTER TABLE `opiniones`
-  ADD PRIMARY KEY (`id_opinion`),
-  ADD UNIQUE KEY `UK9r3mb142o6kg2n51x6hmfinym` (`nombre`),
-  ADD KEY `fkdesc` (`id_descripcion`);
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `UK21rk2fn7ui04bfun59fnr8jp9` (`nombre_de_usuario`),
+  ADD KEY `fkcomentario` (`comentario_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -240,22 +239,22 @@ ALTER TABLE `comandos`
   MODIFY `id_comando` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
--- AUTO_INCREMENT de la tabla `descripcion_comandos`
+-- AUTO_INCREMENT de la tabla `comentarios`
 --
-ALTER TABLE `descripcion_comandos`
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT de la tabla `sintaxis_de_comandos`
+--
+ALTER TABLE `sintaxis_de_comandos`
   MODIFY `id_descripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3838;
 
 --
--- AUTO_INCREMENT de la tabla `descripcion_opiniones`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
-ALTER TABLE `descripcion_opiniones`
-  MODIFY `id_descripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
-
---
--- AUTO_INCREMENT de la tabla `opiniones`
---
-ALTER TABLE `opiniones`
-  MODIFY `id_opinion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
